be67856ef157e25d1907ff8727b7bdbc9b256245
Mughith <mughith.zabibi@gmail.com>
2025-01-02 23:43:38 +0400

    feat: add reservations CRUD

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from '../config/config.module';
 @Module({
@@ -13,4 +13,8 @@ import { ConfigModule } from '../config/config.module';
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
