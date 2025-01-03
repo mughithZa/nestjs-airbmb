45350920a252150348521fb1a7435867337f014c
Mughith <mughith.zabibi@gmail.com>
2025-01-04 00:06:31 +0400

    feat: add validation for reservation dto

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,18 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   userId: string;
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
